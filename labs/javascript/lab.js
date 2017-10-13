ccd26245bddb186b5667b44f5f8da5b96fab724b
wynhsu <wynhsu@uw.edu>
2017-10-13 11:43:35 -0700

    starting lab

--- labs/javascript/lab.js
+++ labs/javascript/lab.js
@@ -1,2 +1,14 @@
 // Put your JavaScript lab code here!
+let x = 'I like JavaScript more than I like to party';
+x = x.slice(27, 42);
+let y = 'I love Washington State University';
+y = y.replace('Washington State University', 'The University of Washington');
+let z = x.concat(y);
 
+sumUpTo (n) {
+    let sum = 0;
+    for (let i = 0; i <= n; i++) {
+        sum += i;
+    }
+    return sum;
+}
