47196d74e724544e30767aa56b481fbf0bc8c2e4
wynhsu <wynhsu@uw.edu>
2017-10-20 00:13:54 -0700

    10/19

--- dom/js/index.js
+++ dom/js/index.js
@@ -30,7 +30,7 @@ const CURRENT_PAGE = document.querySelector("#current-page");
 const TOTAL_PAGES = document.querySelector("#total-pages");
 
 //sort records by name ascending
-BABYNAMES.sort(function(rec1, rec2) {
+BABYNAMES.sort(function (rec1, rec2) {
     return rec1.name.localeCompare(rec2.name);
 });
 
@@ -42,6 +42,9 @@ BABYNAMES.sort(function(rec1, rec2) {
 //When the state changes, we re-render the UI.
 //User actions trigger changes to the state, which
 //triggers a re-render of the UI.
+/**
+ * @property {number} currentPage
+ */
 let state = {
     records: BABYNAMES, //the records to render (might be filtered)
     currentPage: 0      //the current page we are showing
@@ -50,17 +53,73 @@ let state = {
 //TODO: implement functions to render the current state
 //to the page as new <tr> and <td> elements within the
 //<tbody> element that is already in the page.
+/**
+ * Creates and returns a new DOM element
+ * @param {string} name 
+ * @param {*} value 
+ * @param {string} className 
+ */
+function createElem(name, value, className) {
+    let elem = document.createElement(name);
+    elem.textContent = value;
+    if (className) { //If not an empty string 'falsy' value
+        elem.className = className;
+    }
+    return elem;
+}
 
+/**
+ * Renders a record as a table row
+ * @param {BabyNameRecord} record 
+ */
+function renderTableRow(record) {
+    let tr = document.createElement('tr');
+    tr.appendChild(createElem('td', record.name, ''));
+    tr.appendChild(createElem('td', record.sex, ''));
+    tr.appendChild(createElem('td', record.count, 'text-right'));
+    return tr;
+}
 
+function render(state) {
+    let tbody = document.querySelector('tbody');
+    tbody.textContent = '';
+    let totalPages = Math.ceil(state.records.length / PAGE_SIZE);
+    let startingIndex = state.currentPage * PAGE_SIZE;
+    let pageRecords = state.records.slice(startingIndex, startingIndex + PAGE_SIZE);
+    for (let i = 0; i < pageRecords.length; i++) {
+        tbody.appendChild(renderTableRow(pageRecords[i]));
+    }
 
+    CURRENT_PAGE.textContent = state.currentPage + 1;
+    TOTAL_PAGES.textContent = totalPages.toString();
+    PREV_PAGE_BUTTON.disabled = state.currentPage === 0;
+    NEXT_PAGE_BUTTON.disabled = state.currentPage === totalPages - 1;
+}
+
+render(state);
 
 //TODO: listen for the "click" event raised by the 
 //prev/next page buttons, mutate the state.currentPage,
 //and re-render
+NEXT_PAGE_BUTTON.addEventListener('click', function () {
+    state.currentPage++;
+    render(state);
+});
 
+PREV_PAGE_BUTTON.addEventListener('click', function () {
+    state.currentPage--;
+    render(state);
+})
 
 //TODO: listen for the "input" event raised by the
 //name filter <input> element, filter the state.records,
 //and re-render
 
-
+NAME_FILTER_INPUT.addEventListener('input', function () {
+    let nameInput = NAME_FILTER_INPUT.value.trim().toLowerCase();
+    state.records = BABYNAMES.filter(function (record) {
+        return record.name.toLowerCase().startsWith(nameInput);
+    });
+    state.currentPage = 0;
+    render(state);
+});
